bb807d742f744b95e884e013c8262b67cc7a7d46
Piyush Kumar <kpiyush1709@gmail.com>
2018-04-02 12:25:00 +0530

    Added eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "rules":{
+      "linebreak-style": 0, // for windows CRLF
+      "no-console": 0,
+      "consistent-return": 0
+  },
+  "parserOptions": {
+      "sourceType": "module"
+  },
+  env: {
+      node: true,
+  },
+};
